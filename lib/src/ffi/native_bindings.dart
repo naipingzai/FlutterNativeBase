40d395d59733f223adb068d5fcd2ec2402e6e94b
naipingzai <naipingzai@gmail.com>
2026-09-07 17:06:54 +0800

    fix: use DynamicLibrary.open() for reliable native lib loading

--- lib/src/ffi/native_bindings.dart
+++ lib/src/ffi/native_bindings.dart
@@ -1,14 +1,40 @@
 /// FFI function declarations and library loading.
 library;
 
+import 'dart:io';
+import 'dart:ffi';
 import 'package:ffi/ffi.dart';
 
-import 'native_bridge_bindings_generated.dart' as bindings;
+/// Load the native library.
+DynamicLibrary _loadLib() {
+  if (Platform.isLinux) {
+    return DynamicLibrary.open('libnpz_flutter_code.so');
+  } else if (Platform.isAndroid) {
+    return DynamicLibrary.open('libnpz_flutter_code.so');
+  } else if (Platform.isWindows) {
+    return DynamicLibrary.open('npz_flutter_code.dll');
+  } else if (Platform.isMacOS || Platform.isIOS) {
+    return DynamicLibrary.open('libnpz_flutter_code.dylib');
+  }
+  throw UnsupportedError('Unsupported platform: ${Platform.operatingSystem}');
+}
+
+final _lib = _loadLib();
 
 /// Returns the version string of the native bridge library.
-String get version =>
-    bindings.native_bridge_version().cast<Utf8>().toDartString();
+String get version {
+  final fn = _lib.lookupFunction<
+    Pointer<Char> Function(),
+    Pointer<Char> Function()
+  >('native_bridge_version');
+  return fn().cast<Utf8>().toDartString();
+}
 
 /// Returns platform info string (os|arch|compiler|os_version).
-String get platformInfo =>
-    bindings.get_platform_info().cast<Utf8>().toDartString();
+String get platformInfo {
+  final fn = _lib.lookupFunction<
+    Pointer<Char> Function(),
+    Pointer<Char> Function()
+  >('get_platform_info');
+  return fn().cast<Utf8>().toDartString();
+}
